bf99c08542713f2a335a60d55cafab120018ac6b
mikiZhu <mcspero123@gmail.com>
2021-05-14 10:49:30 +0800

    update

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+repository_ssh_url=git@github.com:mikaizhu/Notes.git
+# we can use command : git remote -v to see what url is
+git remote set-url origin $repository_ssh_url
 git add .
 echo "input commit reason..."
 read reason
 git commit -m "$reason"
-git push origin master --force
+git push origin master
